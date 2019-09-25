079a60b4af1d87dc4a4f15966762ff927aa65b7a
Leonardo Gustavo Maran <leonardogmaran9@gmail.com>
2019-09-25 16:13:19 -0300

    Update Aula 4

---
+++ reactnative/hunt/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Routes from './routes';
+
+import React from 'react'
+import { View, Text } from 'react-native'
+import routes from './routes';
+
+// isto aqui que eu estou fazendo aqui
+
+const App = () => <Routes />;
+
+// é como se eu fizesse isto 
+
+// class App extends Component {
+//     render() {
+//         return <Routes />
+//     }
+// }
+
+
+
+
+
